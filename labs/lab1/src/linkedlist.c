a765f1744be3c6fbc0497986800bae7fd632a345
Kanoa Haley <kanoa@kanoa.ca>
2017-05-27 17:42:09 -0400

    Updated lab1 linkedlist implementation

--- labs/lab1/src/linkedlist.c
+++ labs/lab1/src/linkedlist.c
@@ -9,10 +9,18 @@
 #include <stdlib.h>
 #include "../include/LinkedListAPI.h"
 
+/**Function to point the list head to the appropriate functions. Allocates memory to the struct.
+ *@return pointer to the list head
+ *@param printFunction function pointer to print a single node of the list
+ *@param deleteFunction function pointer to delete a single piece of data from the list
+ *@param compareFunction function pointer to compare two nodes of the list in order to test for equality or order
+ **/
 List *initializeList(void (*printFunction)(void *toBePrinted),void (*deleteFunction)(void *toBeDeleted),int (*compareFunction)(const void *first,const void *second))
 {
     List *list = malloc(sizeof(List));
-    list->delateData = deleteFunction;
+    list->head = NULL;
+    list->tail = NULL;
+    list->deleteData = deleteFunction;
     list->compare = compareFunction;
     list->printData = printFunction;
     return list;
@@ -30,7 +38,9 @@ Node *initializeNode(void *data)
 {
     Node *node = malloc(sizeof(Node));
     if (node != NULL) {
-        node->date = data;
+        node->next = NULL;
+        node->previous = NULL;
+        node->data = data;
     }
     return node;
 }
@@ -42,7 +52,15 @@ Node *initializeNode(void *data)
 **/
 void insertFront(List *list, void *toBeAdded)
 {
-    
+    Node *newNode = initializeNode(toBeAdded);
+    newNode->next = list->head;
+    if (list->head != NULL) {
+        list->head->previous = newNode;
+    }
+    list->head = newNode;
+    if (list->tail == NULL) {
+        list->tail = list->head;
+    }
 }
 
 /**Inserts a Node to the back of a linked list. The list then updates accordingly to adhere to the ADT.
@@ -52,7 +70,15 @@ void insertFront(List *list, void *toBeAdded)
 **/
 void insertBack(List *list, void *toBeAdded)
 {
-    
+    Node *newNode = initializeNode(toBeAdded);
+    newNode->previous = list->tail;
+    if (list->tail != NULL) {
+        list->tail->next = newNode;
+    }
+    list->tail = newNode;
+    if (list->head == NULL) {
+        list->head = list->tail;
+    }
 }
 
 /** Deletes the entire linked list head to tail, starting with the nodes, followed by the list itself.
@@ -61,7 +87,14 @@ void insertBack(List *list, void *toBeAdded)
 **/
 void deleteList(List *list)
 {
-    
+    Node *node = list->head;
+    while (node != NULL) {
+        list->deleteData(node->data);
+        Node *tmpNode = node;
+        node = node->next;
+        free(tmpNode);
+    }
+    free(list);
 }
 
 /** Uses the comparison function in the List struct to place the element in the appropriate position in the list.
@@ -74,10 +107,29 @@ as a pointer to the first and last element of the list.
 **/
 void insertSorted(List *list, void *toBeAdded)
 {
-    
+    Node *newNode = initializeNode(toBeAdded);
+    Node *node = list->head;
+    if (node == NULL) {
+        insertFront(list, node);
+    } else {
+        while (node != NULL) {
+            if (list->compare(node->data, newNode->data) >= 0) {
+                newNode->next = node;
+                newNode->previous = node->previous;
+                if (node->previous != NULL) {
+                    node->previous->next = newNode;
+                } else {
+                    list->head = newNode;
+                }
+                node->previous = node;
+                return;
+            }
+            node = node->next;
+        }
+    }
 }
 
-/**Function to remove a node from the list and alter the pointers accordingly to not disrupt the order of the 
+/**Function to remove a node from the list and alter the pointers accordingly to not disrupt the order of the
  *data structure. 
  *@pre List must exist and have memory allocated to it
  *@post toBeDeleted will have its memory freed if it exists in the list.
@@ -87,7 +139,30 @@ void insertSorted(List *list, void *toBeAdded)
  **/
 int deleteDataFromList(List *list, void *toBeDeleted)
 {
-    
+    Node *node = list->head;
+    if (node == NULL) {
+        return EXIT_FAILURE;
+    }
+    while (node != NULL) {
+        if (node->data == toBeDeleted) {
+            if (node->previous != NULL) {
+                node->previous->next = node->next;
+            } else {
+                list->head = node->next;
+            }
+            if (node->next != NULL) {
+                node->next->previous = node->previous;
+            } else {
+                list->tail = node->previous;
+            }
+            list->deleteData(toBeDeleted);
+            free(node);
+            return EXIT_SUCCESS;
+            
+        }
+        node = node->next;
+    }
+    return -1;
 }
 
 /**Function to return the data at the front of the list.
@@ -97,7 +172,7 @@ int deleteDataFromList(List *list, void *toBeDeleted)
  **/
 void *getFromFront(List *list)
 {
-    
+    return list->head == NULL ? NULL : list->head->data;
 }
 
 /**Function to return the data at the back of the list.
@@ -107,7 +182,7 @@ void *getFromFront(List *list)
  **/
 void *getFromBack(List *list)
 {
-    
+    return list->tail == NULL ? NULL : list->tail->data;
 }
 
 /**Function to print list from head to tail. This will utilize the list's printData function pointer to print.
@@ -116,7 +191,11 @@ void *getFromBack(List *list)
  **/
 void printForward(List *list)
 {
-    
+    Node *node = list->head;
+    while (node != NULL) {
+        list->printData(node->data);
+        node = node->next;
+    }
 }
 
 /**Function to print list from tail to head. This will utilize the list's printData function pointer to print.
@@ -125,5 +204,9 @@ void printForward(List *list)
  **/
 void printBackwards(List *list)
 {
-    
+    Node *node = list->tail;
+    while (node != NULL) {
+        list->printData(node->data);
+        node = node->previous;
+    }
 }
