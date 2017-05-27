6e48cbc02c59c550c17797f50854c1f61dd09b7c
Kanoa Haley <kanoa@kanoa.ca>
2017-05-27 17:49:22 -0400

    Added docs and test suite

---
+++ labs/lab1/docs/html/search/files_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['linkedlist_2ec',['linkedlist.c',['../linkedlist_8c.html',1,'']]],
+  ['linkedlistapi_2eh',['LinkedListAPI.h',['../LinkedListAPI_8h.html',1,'']]]
+];
---
+++ labs/lab1/docs/html/search/functions_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['deletedatafromlist',['deleteDataFromList',['../LinkedListAPI_8h.html#ae1a14b22705ddc68f907e6732f4d0843',1,'deleteDataFromList(List *list, void *toBeDeleted):&#160;linkedlist.c'],['../linkedlist_8c.html#ae1a14b22705ddc68f907e6732f4d0843',1,'deleteDataFromList(List *list, void *toBeDeleted):&#160;linkedlist.c']]],
+  ['deletelist',['deleteList',['../LinkedListAPI_8h.html#abc18ab05ec8bd1b4ea085d5b30baf536',1,'deleteList(List *list):&#160;linkedlist.c'],['../linkedlist_8c.html#abc18ab05ec8bd1b4ea085d5b30baf536',1,'deleteList(List *list):&#160;linkedlist.c']]]
+];
