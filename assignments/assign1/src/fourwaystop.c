6b98c5af7457288bf82c61092e607c6482517a5a
Kanoa Haley <kanoa@kanoa.ca>
2017-06-02 20:57:18 -0400

    added main file

---
+++ assignments/assign1/src/fourwaystop.c
@@ -0,0 +1,17 @@
+/**
+ * @file fourwaystop.c
+ * @author Kanoa Haley
+ * @date June 2, 2017
+ * @brief File containing the main program for simulating a four way stop sign
+ *        using a queue ADT from an input file
+ */
+
+ #include <stdio.h>
+
+ #include "../include/QueueAPI.h"
+
+ int main(int argc, char *argv[])
+ {
+
+     return 0;
+ }
