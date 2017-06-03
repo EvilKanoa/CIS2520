9dbc448ce1a8ac4418d1ac4337f2e02b2fb83cf8
Kanoa Haley <kanoa@kanoa.ca>
2017-06-02 21:19:49 -0400

    improved queue api

--- assignments/assign1/include/QueueAPI.h
+++ assignments/assign1/include/QueueAPI.h
@@ -14,8 +14,29 @@
 typedef struct QueueStruct {
     List *list;
     Node *front;
-    Node *back;
     int length;
 } Queue;
+
+/**
+ * Function to create a new queue backed by a linked list
+ * @post a queue is created empty
+ * @return newly created empty queue
+ */
+Queue *queueCreate();
+
+/**
+ * Function to destory and free a previously created queue
+ * @pre queue must be a created queue
+ * @post queue is destoryed and all associated memory is freed
+ */
+void queueDestroy(Queue *queue);
+
+void enqueue(Queue *queue, void *data);
+
+void *dequeue(Queue *queue);
+
+void *queuePeak(Queue *queue);
+
+int queueLength(Queue *queue);
 
  #endif
