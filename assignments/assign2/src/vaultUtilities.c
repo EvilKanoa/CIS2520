9b35bbac6980280f5648bd7ccd7083fb55a9082b
Kanoa Haley <kanoa@kanoa.ca>
2017-06-25 17:31:54 -0400

    Added utility file

---
+++ assignments/assign2/src/vaultUtilities.c
@@ -0,0 +1,14 @@
+/**
+ * @file vaultUtilities.c
+ * @author Kanoa Haley - 0956712 - khaley@mail.uoguelph.ca
+ * @date June 24, 2017
+ * @brief File containing various utilities and hashing functions for the password vault application
+ */
+
+#include <stdlib.h>
+#include "HashTableAPI.h"
+
+int hashDescriptor(char *descriptor)
+{
+    return -1;
+}
