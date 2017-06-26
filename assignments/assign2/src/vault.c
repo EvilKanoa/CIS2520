44e4c3c36efda65f563cdcd44a413c2edb8f99f2
Kanoa Haley <kanoa@kanoa.ca>
2017-06-25 20:39:59 -0400

    Added ncurses

--- assignments/assign2/src/vault.c
+++ assignments/assign2/src/vault.c
@@ -2,15 +2,26 @@
  * @file vault.c
  * @author Kanoa Haley - 0956712 - khaley@mail.uoguelph.ca
  * @date June 24, 2017
- * @brief File containing the implemention for the vault application including its main method
+ * @brief File containing the implemention for the vault application including its main method. Mostly used for the UI.
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <ncurses.h>
 #include "VaultUtilities.h"
+#include "VaultFile.h"
+
+#define CONFIG_FILE "config.txt"
 
 int main()
 {
-    int hash;
-    hash = hashDescriptor(100, "amazon");
-    printf("Hash is: %d\n", hash);
+    initscr();
+    noecho();
+
+    printw("Hello, World!");
+    refresh();
+    getch();
+
+    endwin();
     return 0;
 }
